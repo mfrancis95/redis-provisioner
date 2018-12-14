65bd6fe897029d492db30313374d18d662bce892
mfrancis95 <mikefrancis95@gmail.com>
2018-12-13 19:04:26 -0500

    Remove container if the port is already in use

--- redis_provisioner/redis.py
+++ redis_provisioner/redis.py
@@ -1,24 +1,31 @@
 from docker import APIClient
 from os import environ
-from subprocess import call
+from docker.errors import APIError
 
-_docker = APIClient(environ['DOCKER_SOCKET'], 'auto')
+_docker = APIClient(environ.get('DOCKER_SOCKET', 'unix://var/run/docker.sock'), 'auto')
 
 def _container_map(container):
     return {
-        'id': container['Id'],
-        'name': container['Names'][0][1:],
+        'id': container['Id'], 'name': container['Names'][0][1:],
         'port': container['Ports'][0]['PublicPort'],
         'status': container['Status'],
         'username': container['Labels']['username']
     }
 
 def create(username, name, port, password):
-    return call([
-        'docker', 'run', '-d', '-l' f'username={username}', '--name', name,
-        '-p', f'{port}:6379', 'redis:alpine', 'redis-server', '--requirepass',
-        password
-    ])
+    try:
+        container = _docker.create_container(
+            'redis:alpine', command = f'redis-server --requirepass {password}',
+            detach = True,
+            host_config = _docker.create_host_config(port_bindings = {6379: port}),
+            labels = {'username': username}, name = name
+        )
+        _docker.start(container['Id'])
+        return True
+    except APIError as error:
+        if error.status_code == 500:
+            _docker.remove_container(container['Id'])
+    return False
 
 def get_instances():
     return map(_container_map, _docker.containers())
